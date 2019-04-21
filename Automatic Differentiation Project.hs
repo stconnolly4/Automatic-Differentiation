47a8b2a13f6029668199ee4069c90ce93abb440f
Leslie M VanDeMark <leslie.vandemark@uvm.edu>
2019-04-21 15:26:44 -0400

    added env and expr

--- Automatic Differentiation Project.hs
+++ Automatic Differentiation Project.hs
@@ -12,9 +12,19 @@ import Control.Monad hiding (join) -- used in testing infrastructure
 import Control.Exception           -- used in testing infrastructure
 import System.IO                   -- used in testing infrastructure
 
+data Expr = IntE Integer
+          | PlusE Expr Expr
+          | TimesE Expr Expr
+          | VarE String
+          | LetE String Expr Expr
+
+  deriving (Eq,Ord,Read,Show)
+
 data Value = IntV Integer
   deriving (Eq,Ord,Show)
 
 data Derivative = IntD Integer
   deriving (Eq,Ord,Show)
---git test
+
+-- γ^ ∈ env^ ≜ var ⇀ value^
+type Env = Map String Value'

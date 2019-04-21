cb968e1c3c6fc626a446bf410f56e2bb33c72161
Samantha Connolly <staylorconnolly@gmail.com>
2019-04-21 15:03:27 -0400

    setup initial

--- Automatic Differentiation Project.hs
+++ Automatic Differentiation Project.hs
@@ -1,2 +1,16 @@
--- test
--- sami and leslie
+{-# LANGUAGE GADTs #-} -- used in testing infrastructure
+{-# OPTIONS_GHC -fwarn-incomplete-patterns #-}
+module AutomaticDifferentiation where
+
+import Data.Map (Map)
+import Data.Set (Set)
+import qualified Data.Map as Map
+import qualified Data.Set as Set
+
+import Data.List                   -- used in testing infrastructure
+import Control.Monad hiding (join) -- used in testing infrastructure
+import Control.Exception           -- used in testing infrastructure
+import System.IO                   -- used in testing infrastructure
+
+data Value = IntV Integer
+  deriving (Eq,Ord,Show)

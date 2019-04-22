a507fd8a1cf7989225ba0cf3635e40c0f8fce274
Samantha Connolly <staylorconnolly@gmail.com>
2019-04-22 12:56:15 -0400

    lunch work

--- Automatic Differentiation Project.hs
+++ Automatic Differentiation Project.hs
@@ -3,30 +3,45 @@
 module AutomaticDifferentiation where
 
 import Data.Map (Map)
-import Data.Set (Set)
 import qualified Data.Map as Map
-import qualified Data.Set as Set
 
 import Data.List                   -- used in testing infrastructure
 import Control.Monad hiding (join) -- used in testing infrastructure
 import Control.Exception           -- used in testing infrastructure
 import System.IO                   -- used in testing infrastructure
 
-data Value = FloatV Float
+data Variable = Var String
   deriving (Eq,Ord,Show)
+
+data Value = Value Float
+  deriving (Eq, Ord, Show)
 
 data Derivative = FloatD Float
   deriving (Eq,Ord,Show)
 
 data ValueHat = VHat Value Derivative
+  deriving (Eq,Ord,Show)
 
 type EnvHat = Map String ValueHat
 
-data Expr = IntE Integer
+data Expr = VarE Variable
+          | FloatE Float
           | PlusE Expr Expr
           | TimesE Expr Expr
-          | VarE String
-          | LetE String Expr Expr
+          | SinE Expr
+          | CosE Expr
   deriving (Eq,Ord,Show)
 
-differentiate EnvHat -> Expr -> ValueHat
+differentiate :: EnvHat -> Expr -> Maybe ValueHat
+differentiate env e = case e of
+  FloatE f -> Just (VHat (Value f) (FloatD 0))
+  VarE v -> case v of
+    Var s -> case Map.lookup s env of
+      Just (VHat v1 d1) -> Just (VHat (Value 1) (FloatD 1))
+      Nothing -> Just (VHat (Value 0) (FloatD 0))
+  PlusE e1 e2 -> case differentiate env e1 of
+    Just (VHat e1v e1d) -> case differentiate env e2 of
+      Just (VHat e2v e2d) -> Just (VHat (Value e1v + e2v) (FloatD e1d + e2d))
+      Nothing -> Nothing
+    Nothing -> Nothing
+  _ -> Nothing

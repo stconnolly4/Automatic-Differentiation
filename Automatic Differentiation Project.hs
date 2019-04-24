7c8b1126431da7860eeff28b4f2f7f48374fd5e3
Leslie M VanDeMark <leslie.vandemark@uvm.edu>
2019-04-24 16:22:13 -0400

    testsss

--- Automatic Differentiation Project.hs
+++ Automatic Differentiation Project.hs
@@ -38,7 +38,7 @@ differentiate env e = case e of
   VarE v -> case v of
     Var s -> case Map.lookup s env of
       Just (VHat v1 d1) -> Just (VHat v1 d1)
-      Nothing -> Just (VHat (ValueD 0) (DerivativeD 0))
+      Nothing -> Nothing
   PlusE e1 e2 -> case differentiate env e1 of
     Just (VHat (ValueD e1v) (DerivativeD e1d)) -> case differentiate env e2 of
       Just (VHat (ValueD e2v) (DerivativeD e2d)) -> Just (VHat (ValueD (e1v + e2v)) (DerivativeD (e1d + e2d)))
@@ -56,11 +56,110 @@ differentiate env e = case e of
     Just (VHat (ValueD e1v) (DerivativeD e1d)) -> Just (VHat (ValueD (cos e1v)) (DerivativeD (-e1d * (sin e1v))))
     Nothing -> Nothing
 
-  differentiateTests :: (Int,String,EnvHat,Expr -> Maybe ValueHat,[(EnvHat,Expr,Maybe ValueHat)])
-  differentiateTests =
-    ( 1
-    , "differentiate"
-    , differentiate
-    , [(Map.empty,DoubleE 5, ValueHat (ValueV 5) (ValueD 0))
-      ]
-    )
+differentiateTests :: (Int,String,(EnvHat -> Expr -> Maybe ValueHat),[((EnvHat,Expr),Maybe ValueHat)])
+differentiateTests =
+  ( 1
+  , "differentiate"
+  , differentiate
+  , [(  (Map.empty,DoubleE 5),
+        Just (VHat (ValueD 5) (DerivativeD 0))
+     )
+     -- (  input,
+     --    output
+     -- )
+    ]
+  )
+
+---------------
+-- ALL TESTS --
+---------------
+
+allTests :: [Test]
+allTests =
+  [ Test2 differentiateTests
+  ]
+
+----------------------
+-- MAIN = RUN TESTS --
+----------------------
+
+main :: IO ()
+main = runTests allTests
+
+----------------------------
+-- TESTING INFRASTRUCTURE --
+----------------------------
+
+mapOn :: [a] -> (a -> b) -> [b]
+mapOn = flip map
+
+foldMOn :: (Foldable t,Monad m) => b -> t a -> (b -> a -> m b) -> m b
+foldMOn i xs f = foldM f i xs
+
+data Test where
+  Test1 :: (Show a,Eq b,Show b) => (Int,String,a -> b,[(a,b)]) -> Test
+  Test2 :: (Show a,Show b,Eq c,Show c) => (Int,String,a -> b -> c,[((a,b),c)]) -> Test
+  Test3 :: (Show a,Show b,Show c,Eq d,Show d) => (Int,String,a -> b -> c -> d,[((a,b,c),d)]) -> Test
+
+runTests :: [Test] -> IO ()
+runTests ts = do
+  rs <- forM ts $ \ t -> do
+    y <- case t of
+      Test1 t -> runTests1 t
+      Test2 t -> runTests2 t
+      Test3 t -> runTests3 t
+    putStrLn ""
+    return y
+  forM_ (zip [1..] rs) $ \ (m,(n,passed,failed)) -> do
+    when (m /= 1) $ putStrLn ""
+    putStrLn $ "++ E" ++ show n ++ " Tests Passed: " ++ show passed
+    putStrLn $ "-- E" ++ show n ++ " Tests Failed: " ++ show failed
+
+showTestResult :: (Eq a,Show a) => String -> a -> Either String a -> (Int,Int) -> IO (Int,Int)
+showTestResult fx y y'M (passed,failed) = do
+  let eM = case y'M of
+        Left e -> Just $ "[ERROR]: " ++ e
+        Right y' ->
+          if y' == y
+          then Nothing
+          else Just $ show y'
+  case eM of
+    Nothing -> do
+      putStrLn $ "   [TEST PASSED]: " ++ fx
+      hFlush stdout
+      return (passed+1,failed)
+    Just s -> do
+      putStrLn $ "   [TEST FAILED]:"
+      putStrLn $ "     -- the input"
+      putStrLn $ "     " ++ fx
+      putStrLn $ "   =="
+      putStrLn $ "     -- the output"
+      putStrLn $ "     " ++ s
+      putStrLn $ "   /="
+      putStrLn $ "     -- the expected result"
+      putStrLn $ "     " ++ show y
+      hFlush stdout
+      return (passed,failed+1)
+
+runTestsN :: (Eq a,Show a) => Int -> String -> [(String,() -> a,a)] -> IO (Int,Int,Int)
+runTestsN n name tests = do
+  putStrLn $ ">> E" ++ show n ++ " Tests: " ++ name
+  (passed,failed) <- foldMOn (0,0) tests $ \ pf (s,fx,y) -> do
+    y'M <- catch (Right <$> evaluate (fx ())) $ \ (SomeException e) -> return $ Left $ chomp $ unwords $ lines $ show e
+    showTestResult s y y'M pf
+  return (n,passed,failed)
+  where
+    chomp s0 = concat $ mapOn (group s0) $ \ s ->
+      if " " `isPrefixOf` s then " " else s
+
+runTests1 :: (Eq b,Show a,Show b) => (Int,String,a -> b,[(a,b)]) -> IO (Int,Int,Int)
+runTests1 (n,name,f,tests) = runTestsN n name $ mapOn tests $ \ (x,y) ->
+  (name ++ " " ++ showsPrec 11 x [],\() -> f x,y)
+
+runTests2 :: (Eq c,Show a,Show b,Show c) => (Int,String,a -> b -> c,[((a,b),c)]) -> IO (Int,Int,Int)
+runTests2 (n,name,f,tests) = runTestsN n name $ mapOn tests $ \ ((x,y),z) ->
+  (name ++ " " ++ showsPrec 11 x [] ++ " " ++ showsPrec 11 y [],\() -> f x y,z)
+
+runTests3 :: (Eq d,Show a,Show b,Show c,Show d) => (Int,String,a -> b -> c -> d,[((a,b,c),d)]) -> IO (Int,Int,Int)
+runTests3 (n,name,f,tests) = runTestsN n name $ mapOn tests $ \ ((w,x,y),z) ->
+  (name ++ " " ++ showsPrec 11 w [] ++ " " ++ showsPrec 11 x [] ++ " " ++ showsPrec 11 y [],\() -> f w x y,z)

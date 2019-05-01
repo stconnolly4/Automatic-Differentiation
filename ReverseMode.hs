7bfe53a4c7b43b2c7952ea93213facd183a9fa7c
Leslie M VanDeMark <leslie.vandemark@uvm.edu>
2019-05-01 16:20:36 -0400

    initial commit

--- ReverseMode.hs
+++ ReverseMode.hs
@@ -16,34 +16,34 @@ data Variable = Var String
 data Value = ValueD Double
   deriving (Eq, Ord, Show)
 
--- data Derivative = DerivativeD (Double -> Double)
---   deriving (Eq,Ord,Show)
+data Derivative = DerivativeD (Double -> Double)
 
-data Derivative = DerivativeD Double
+data FMDerivative = FMDerivativeD Double
   deriving (Eq,Ord,Show)
 
-derivative :: (Double -> Double)
-derivative d = undefined
 
 data ValueHat = VHat Value Derivative
+
+data FMValueHat = FMVHat Value FMDerivative
   deriving (Eq,Ord,Show)
 
 type EnvHat = Map String ValueHat
 
 data Expr = DoubleE Double
+          | VarE Variable
           | SinE Expr
   deriving (Eq,Ord,Show)
 
-
+drive :: ValueHat -> FMValueHat
+drive (VHat v (DerivativeD f)) = FMVHat v (FMDerivativeD (f 1))
 
 --Use forward and instead of getting parent derivative use function in that place to send to child
 
 differentiate :: EnvHat -> Expr -> Maybe ValueHat -- we should be producing a value wihtout needing parents derivative, and then we\
 differentiate env e = case e of
-  --SinE e1 -> differentiate env e1 (DerivativeD (cos d1))
-  --DoubleE e1 -> VHat (ValueD e1) d
-  DoubleE f -> Just (VHat (ValueD f) (DerivativeD 0))
+  DoubleE f -> Just (VHat (ValueD f) (DerivativeD (\0->0)))
+  VarE v -> case v of
+    Var s -> Map.lookup s env
   SinE e1 -> case differentiate env e1 of
-    Just (VHat (ValueD e1v) (DerivativeD e1d)) -> Just (VHat (ValueD (sin e1v)) (\  -> e1d * cos (e1v)) )
-    --(DerivativeD (e1d * (cos e1v))))
+    Just (VHat (ValueD e1v) (DerivativeD e1d)) -> Just (VHat (ValueD (sin e1v)) (DerivativeD (\d -> -d * sin(e1v))))
     Nothing -> Nothing
